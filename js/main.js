77c22468ff09834b4f70721aa9caff9c9c1820d8
naf <naf@auri.ga>
2018-04-29 22:10:33 +0900

    はじめの一歩

---
+++ js/main.js
@@ -0,0 +1,57 @@
+console.log("Mastoshare Ver 2.0.0 alpha!");
+load();
+function load() {
+    var an = "<option value=\"mstdn.jp\">mstdn.jp</option><option value=\"friends.nico\">friends.nico</option><option value=\"pawoo.net\">pawoo.net</option>";
+    for (var i = 0, length = localStorage.length; i < length; ++i) {
+        an += "<option value='" + localStorage[i] + "'>" + localStorage[i] + "</option>";
+    }
+    document.getElementById('instance').innerHTML = an;
+}
+function check(_url) {
+    let xhr;
+    xhr = new XMLHttpRequest();
+    xhr.open("GET", _url, false);  //同期モード
+    xhr.send(null);
+    return xhr.status;
+}
+document.addEventListener("DOMContentLoaded", function () {
+
+    document.getElementById("addlistbtn").addEventListener("click", function () {
+        var addInstanceurl = document.getElementById("addlist").value;
+        var checkUrl = "https://" + document.getElementById("addlist").value + "/api/v1/instance";
+        try {
+            if (check(checkUrl) === 200) {
+                let option = document.createElement("option");
+                option.value = addInstanceurl;
+                option.text = addInstanceurl;
+                let listNumber = localStorage.length.toString();
+                localStorage.setItem(listNumber, addInstanceurl);
+                let target = document.getElementsByName("instance")[0];
+                target.add(option);
+            } else {
+                msg="<div class=\"alert alert-danger error\" role=\"alert\">マストドンインスタンス(v1.6.0以上)ではありません</div>";
+                document.getElementById('error').innerHTML = msg;
+            }
+        }catch (e){
+            msg="<div class=\"alert alert-danger error\" role=\"alert\">不正なアドレスです</div>";
+            document.getElementById('error').innerHTML = msg;
+        }
+
+    });
+
+    document.getElementById("Toot").addEventListener("click", function () {
+        let text = document.getElementById("contents").value;
+        let instanceUrl = document.getElementById("instance").value;
+        if (instanceUrl === ""){
+                msg="<div class=\"alert alert-danger error\" role=\"alert\">共有するインスタンスを選択してください</div>";
+                document.getElementById('error').innerHTML = msg;
+        }else {
+            let openUrl = encodeURIComponent(text);
+            window.open("https://" + instanceUrl + "/share?text=" + openUrl, '_blank');
+        }
+    });
+    document.getElementById("del").addEventListener("click", function () {
+        localStorage.clear();
+        load();
+    });
+});
---
+++ post.php
@@ -0,0 +1,71 @@
+<?php
+$get = isset($_GET['text']) ? $_SERVER['text'] : '';
+$referer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : null;
+if (!is_null($referer)){
+    if(preg_match("|^https?://shindanmaker\.com|", $referer)) {
+        $text = $get;
+    }else{
+        if($data = @file_get_contents($referer)){
+            //データ取得が成功した
+            $data = mb_convert_encoding($data, 'HTML-ENTITIES', 'ASCII, JIS, UTF-8, EUC-JP, SJIS');
+            $document = new DOMDocument();
+            $document->loadHTML($data);
+            $xmlstr = $document->saveXML();
+            $xmlobj = simplexml_load_string($xmlstr);
+            $page = json_decode(json_encode($xmlobj), true);
+            if (!$page['head']['title']) {
+                $text = $get."\n".$page['head']['title'] . " " . $referer." から #Mastoshare";
+            }else{
+                $text = $get."\n".$referer." から #Mastoshare";
+            }
+        }else{
+            $text = $get."\n".$referer." から #Mastoshare";
+        }
+    }
+}else{
+    $text = $get."\n#Mastoshare";
+}
+
+?>
+<!doctype html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Mastoshare</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.dark.min.css">
+    <link rel="stylesheet" type="text/css" href="css/styel.css">
+</head>
+
+<div class="container">
+    <h1>Mastoshare</h1>
+    <div id="error"></div>
+    <div class="card">
+        <div class="card-body">
+            <h4>リストへの追加</h4>
+            <div class="row">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="basic-addon1">https://</span>
+                    </div>
+                    <input type="text" class="form-control" id="addlist" placeholder="mstdn.jp">
+                    <button id="addlistbtn" class="btn btn-primary">追加</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <h4>インスタンスリスト</h4>
+    <div class="form-group">
+        <select class="form-control" id="instance" name="instance" size="5" required>
+        </select>
+    </div>
+    <div class="form-group">
+        <textarea id="contents" class="form-control" name="num" placeholder="Toot内容" rows="3" required><?php echo $text;?></textarea>
+    </div>
+    <button id="Toot" class="btn btn-primary">Toot</button>
+    <button id="del" class="btn btn-danger">リスト全削除</button>
+</div>
+<script src="js/main.js"></script>
+</body>
+
+</html>
