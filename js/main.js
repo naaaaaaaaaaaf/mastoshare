f75eb85d00a943e2bb352806c6263a176457d644
Karno <karnoroid@gmail.com>
2018-12-08 19:06:16 +0900

    remembering last-selected instance.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 console.log("Mastoshare Ver 2.0.0 alpha!");
 load();
 function load() {
-    var an = "<option value=\"mstdn.jp\">mstdn.jp</option><option value=\"friends.nico\">friends.nico</option><option value=\"pawoo.net\">pawoo.net</option>";
-    for (var i = 0, length = localStorage.length; i < length; ++i) {
-        an += "<option value='" + localStorage[i] + "'>" + localStorage[i] + "</option>";
+    let an = "<option value=\"mstdn.jp\">mstdn.jp</option><option value=\"friends.nico\">friends.nico</option><option value=\"pawoo.net\">pawoo.net</option>";
+    let length = localStorage.length;
+    let lastSelect = localStorage.getItem("lastSelected");
+    if (lastSelect != null) {
+        length -= 1;
+    }
+    for (let i = 0; i < length; ++i) {
+        let instance = localStorage[i];
+        let selected = lastSelect == instance ? " selected='selected'" : "";
+        an += "<option value='" + instance + "'" + selected + ">" + instance + "</option>";
+
     }
     document.getElementById('instance').innerHTML = an;
 }
@@ -24,8 +32,11 @@ document.addEventListener("DOMContentLoaded", function () {
                 let option = document.createElement("option");
                 option.value = addInstanceurl;
                 option.text = addInstanceurl;
-                let listNumber = localStorage.length.toString();
-                localStorage.setItem(listNumber, addInstanceurl);
+                let listNumber = localStorage.length;
+                if (localStorage.getItem("lastSelected") != null) {
+                    listNumber -= 1;
+                }
+                localStorage.setItem(listNumber.toString(), addInstanceurl);
                 let target = document.getElementsByName("instance")[0];
                 target.add(option);
             } else {
@@ -46,6 +57,7 @@ document.addEventListener("DOMContentLoaded", function () {
             msg = "<div class=\"alert alert-danger error\" role=\"alert\">共有するインスタンスを選択してください</div>";
             document.getElementById('error').innerHTML = msg;
         } else {
+            localStorage.setItem("lastSelected", instanceUrl);
             let openUrl = encodeURIComponent(text);
             location.href = "https://" + instanceUrl + "/share?text=" + openUrl;
         }
