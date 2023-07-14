985e9524a5fb57fa8d5a4c6ca4bd4ae8a5ea05fc
Takato Ueno <naf@auriga.dev>
2023-07-15 01:13:20 +0900

    share.phpのパスにも対応

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
     <Router>
       <Routes>
       <Route path="/share" element={<ShareComponent />} />
+      <Route path="/share.php" element={<ShareComponent />} />
       <Route path="/settings" element={<SettingsComponent />} />
         {/* 他のルートをここに追加します */}
       </Routes>
